a8e73097b824b6d2441fefef47a93b6b5d5fe296
luciano dlr <delarubialuciano@gmail.com>
2022-06-28 00:05:30 -0300

    using,firestore,console.log

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -2,44 +2,55 @@ import { useState, useEffect } from 'react'
 import React from 'react'
 import ItemDetail from './ItemDetail'
 import { useParams } from 'react-router-dom'
-const productos = require('../../asyncmock').default;
+// const productos = require('../../asyncmock').default;
+
+import { collection, getDocs } from "firebase/firestore";
+import db from '../../firebase';
 
 
-const ItemDetailContainer = () =>{
+const ItemDetailContainer = () => {
     const [dato, setDato] = useState({});
-    const {id} = useParams();
+    const { id } = useParams();
     const [loading, setLoading] = useState(false);
-    
-    
-    useEffect(()=>{
-    let isok = true;
-    
-    const data = (time, task) => {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                if (isok) {
-                    resolve(task);
-                } else {
-                    reject("Error setDato");
-                }
-            }, time);
-        });
-    }
-    setLoading(true)
-    data(200, productos.find(item => item.id === parseInt(id)))
-        .then((res) => setDato(res))
-        .catch(err => console.log(err))
-        .finally(()=>setLoading(false))
-    },[id]);
-    
-    
-        return (
-            <>
-                {loading ? <h1>Cargando...</h1> : <ItemDetail item={dato}/>}
-            
-            </>
-    
-        )
-    }
+
+
+    useEffect(() => {
+        // let isok = true;
+
+        // const data = (time, task) => {
+        //     return new Promise((resolve, reject) => {
+        //         setTimeout(() => {
+        //             if (isok) {
+        //                 resolve(task);
+        //             } else {
+        //                 reject("Error setDato");
+        //             }
+        //         }, time);
+        //     });
+        // }
+        // setLoading(true)
+        // data(200, productos.find(item => item.id === parseInt(id)))
+        //     .then((res) => setDato(res))
+        //     .catch(err => console.log(err))
+        //     .finally(() => setLoading(false))
+        const firebaseFetch = async () => {
+            const querySnapshot = await getDocs(collection(db, "products"));
+            querySnapshot.forEach((doc) => {
+                console.log(`${doc.id} => ${doc.data()}`);
+            });
+        };
+        firebaseFetch();
+
+    }, [id]);
+
+
+    return (
+        <>
+            {loading ? <h1>Cargando...</h1> : <ItemDetail item={dato} />}
+
+        </>
+
+    )
+}
 
 export default ItemDetailContainer;
