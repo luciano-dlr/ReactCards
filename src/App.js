14d50e384b25c023cabbb05f40758ec600b1bc2c
luciano dlr <delarubialuciano@gmail.com>
2022-06-29 18:57:06 -0300

    working,app

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import  './App.css'
 import React from 'react';
-import Navbar from './components/NavBar/NavBar'
+import Navbar from './components/Navbar/Navbar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
 import Cart from './components/Cart/Cart';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import  CartContextProvider  from './components/CartContext';
+import  CartContextProvider  from './components/Context/CartContext';
 
 
 
 
 const App = () => {
-  
-  
+
+
   return ( 
     <CartContextProvider>
       <BrowserRouter>
           <Navbar/>
           <Routes>
-            <Route path='/' element={<ItemListContainer/>}></Route>
-            <Route path='/category/:categoryId' element={<ItemListContainer greeting="Productos filtrados por categoria"/>}/>
-            <Route path='/item/:id' element={<ItemDetailContainer/>}></Route>
+            <Route path='/' element={<ItemListContainer greeting="Todos los productos"/>}></Route>
+            <Route path='/category/:idcategory' element={<ItemListContainer greeting="Productos por categoria"/>}/>
+            <Route path='/item/:idItem' element={<ItemDetailContainer/>}></Route>
             <Route path='/cart' element={<Cart />} />
           </Routes>
       </BrowserRouter>
       </CartContextProvider>
-      
-      
-      );
-    }
-    export default App;
+   );
+}
+export default App;
+
+
+
     // // import logo from './logo.svg';
     // import './App.css';
     // import NavBar from './components/NavBar/NavBar';
